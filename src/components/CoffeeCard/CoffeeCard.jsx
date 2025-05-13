6c90617cf48d8b8c6dc240a6a453e9cf7f2b7b30
VolodX <xpain1996@gmail.com>
2025-05-13 18:07:40 +0300

    feat: create CoffeeCard component with basic structure and styles

---
+++ src/components/CoffeeCard/CoffeeCard.jsx
@@ -0,0 +1,14 @@
+import './CoffeeCard.css';
+
+function CoffeeCard() {
+	return (
+		<li className="coffee-card">
+			<img src="" alt="" className="coffee-card__img" />
+			<h3 className="coffee-card__title"></h3>
+			<p className="coffee-card__subtitle"></p>
+			<p className="coffee-card__price"></p>
+		</li>
+	)
+}
+
+export default CoffeeCard;
