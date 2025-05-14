1550b65d03b3f88ba1187096e614715d54db32e3
VolodX <xpain1996@gmail.com>
2025-05-14 14:29:38 +0300

    feat(components): add styles and props to CoffeeCard component

--- src/components/CoffeeCard/CoffeeCard.jsx
+++ src/components/CoffeeCard/CoffeeCard.jsx
@@ -1,12 +1,12 @@
 import './CoffeeCard.css';
 
-function CoffeeCard() {
+function CoffeeCard({name, origin, price, image}) {
 	return (
 		<li className="coffee-card">
-			<img src="" alt="" className="coffee-card__img" />
-			<h3 className="coffee-card__title"></h3>
-			<p className="coffee-card__subtitle"></p>
-			<p className="coffee-card__price"></p>
+			<img src={image} alt="Aromistico Coffee" className="coffee-card__img" />
+			<h3 className="coffee-card__title">{name}</h3>
+			<p className="coffee-card__origin">{origin}</p>
+			<p className="coffee-card__price">{price}</p>
 		</li>
 	)
 }
