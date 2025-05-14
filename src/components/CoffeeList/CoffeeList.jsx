10a783a7ed663a317285a403efe0dcbb69ae761f
VolodX <xpain1996@gmail.com>
2025-05-14 14:33:50 +0300

    feat(CoffeeList): add new CoffeeList component

---
+++ src/components/CoffeeList/CoffeeList.jsx
@@ -0,0 +1,17 @@
+import './CoffeeList.css';
+import CoffeeCard from '../CoffeeCard/CoffeeCard';
+import { memo } from 'react';
+
+const CoffeeList = ({data}) => {
+  if (data.length === 0) {
+    return <p className="coffee-list__empty tac">No coffee items available.</p>;
+  }
+
+  const elements = data.map(({id, ...itemProps}) => (
+    <CoffeeCard key={id} {...itemProps} />
+  ));
+
+  return <div className="coffee-list__items">{elements}</div>;
+};
+
+export default CoffeeList;
