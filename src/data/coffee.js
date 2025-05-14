577d83622db3d7a2e13b85cc19fe5fca6b908052
VolodX <xpain1996@gmail.com>
2025-05-14 14:18:45 +0300

    refactor(data): update coffee name and image import method in coffeeData.js

--- src/data/coffee.js
+++ src/data/coffee.js
@@ -1,45 +1,48 @@
-// Data about coffee for "Our Coffee" page
-export const coffeeData = [
+import defaultCoffeeImage from '@/assets/images/coffee-items/coffee.jpg'
+
+const coffeeData = [
   {
     id: 1,
-    name: 'Brazilian Coffee',
-    country: 'Brazil',
+    name: 'AROMISTICO Brazil 1 kg',
+    origin: 'Brazil',
     price: 6.99,
-    image: '@/assets/images/coffee-items/coffee.jpg'
+    image: defaultCoffeeImage
   },
   {
     id: 2,
-    name: 'Kenya AA Coffee',
-    country: 'Kenya',
+    name: 'AROMISTICO Kenya 1 kg',
+    origin: 'Kenya',
     price: 6.99,
-    image: '@/assets/images/coffee-items/coffee.jpg'
+    image: defaultCoffeeImage
+  },
+  {
+    id: 3,
+    name: 'AROMISTICO Rwanda 1 kg',
+    origin: 'Rwanda',
+    price: 6.99,
+    image: defaultCoffeeImage
   },
   {
     id: 4,
-    name: 'Colombian Supremo',
-    country: 'Colombia',
+    name: 'AROMISTICO Brazil 1 kg',
+    origin: 'Brazil',
     price: 6.99,
-    image: '@/assets/images/coffee-items/coffee.jpg'
-  },
-  {
-    id: 4,
-    name: 'Brazilian Coffee',
-    country: 'Brazil',
-    price: 6.99,
-    image: '@/assets/images/coffee-items/coffee.jpg'
+    image: defaultCoffeeImage
   },
   {
     id: 5,
-    name: 'Kenya AA Coffee',
-    country: 'Kenya',
+    name: 'AROMISTICO Kenya 1 kg',
+    origin: 'Kenya',
     price: 8.99,
-    image: '@/assets/images/coffee-items/coffee.jpg'
+    image: defaultCoffeeImage
   },
   {
     id: 6,
-    name: 'Colombian Supremo',
-    country: 'Colombia',
+    name: 'AROMISTICO Rwanda 1 kg',
+    origin: 'Rwanda',
     price: 7.99,
-    image: '@/assets/images/coffee-items/coffee.jpg'
+    image: defaultCoffeeImage
   },
 ];
+
+export default coffeeData;
