e6e0cfac8ae38835d97187cd2b2639a9d91d9413
VolodX <xpain1996@gmail.com>
2025-05-14 19:01:20 +0300

    feat(SearchFilter): optimize button rendering and add active style in SearchFilter.jsx

--- src/components/SearchFilter/SearchFilter.jsx
+++ src/components/SearchFilter/SearchFilter.jsx
@@ -1,6 +1,28 @@
 import './SearchFilter.css';
 
-function SearchFilter({onUpdateSearch}) {
+function SearchFilter({filterOrigin, onUpdateSearch, onSelectFilter}) {
+  const originData = [
+    {origin: 'All', label: 'All'},
+    {origin: 'Brazil', label: 'Brazil'},
+    {origin: 'Kenya', label: 'Kenya'},
+    {origin: 'Rwanda', label: 'Rwanda'}
+  ];
+
+  const buttons = originData.map(({origin, label}) => {
+    const active = filterOrigin === origin;
+    const clazz = active ? 'search-filter__region-btn--active' : '';
+
+    return (
+      <button
+        className={`search-filter__region-btn ${clazz}`.trim()}
+        key={origin}
+        onClick={() => onSelectFilter(origin)}
+      >
+        {label}
+      </button>
+    );
+  });
+
   return (
     <div className="search-filter">
       <div className="search-filter__query">
@@ -9,23 +31,17 @@ function SearchFilter({onUpdateSearch}) {
         </label>
         <input
           className="search-filter__input"
-					onChange={(e) => onUpdateSearch(e.target.value)}
+          onChange={e => onUpdateSearch(e.target.value)}
           id="search-input"
           type="text"
           placeholder="start typing here..."
         />
       </div>
 
-			{/* Filter by region */}
+      {/* Filter by region */}
       <div className="search-filter__region">
-        <p className="search-filter__region-title">
-          Or filter
-        </p>
-        <div className="search-filter__region-btns">
-          <button className="search-filter__region-btn">Brazil</button>
-          <button className="search-filter__region-btn">Kenya</button>
-          <button className="search-filter__region-btn">Columbia</button>
-        </div>
+        <p className="search-filter__region-title">Or filter</p>
+        <div className="search-filter__region-btns">{buttons}</div>
       </div>
     </div>
   );
