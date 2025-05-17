de79d43e2eef3ddca3eaa80ef033e7601a6de5e8
Volodymyr Brezitskyi <118307273+VolodX@users.noreply.github.com>
2025-05-17 14:20:52 +0300

    refactor(homepage): update About Us text

--- src/pages/HomePage/AboutUs/AboutUs.jsx
+++ src/pages/HomePage/AboutUs/AboutUs.jsx
@@ -8,18 +8,11 @@ function AboutUs() {
       <Divider variant="black" />
       <div className="about-us__content">
         <p>
-          Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
-          Afraid at highly months do things on at. Situation recommend objection do intention so
-          questions. As greatly removed calling pleased improve an. Last ask him cold feel met
-          spot shy want. Children me laughing we prospect answered followed. At it went is song
-          that held help face.
+          Our story began with a deep love for coffee. We believe that every cup should be a source of energy and true pleasure, transforming an ordinary moment into a small ritual. We are dedicated to finding and selecting the best coffee beans from around the world, working with farmers who share our commitment to quality and sustainability. Our mission is to share this exceptional coffee with you, ensuring every day is filled with unparalleled taste and aroma.
         </p>
         <br />
         <p>
-          Now residence dashwoods she excellent you. Shade being under his bed her, Much read on as
-          draw. Blessing for ignorant exercise any yourself unpacked. Pleasant horrible but
-          confined day end marriage. Eagerness furniture set preserved far recommend. Did even but
-          nor are most gave hope. Secure active living depend son repair day ladies now.
+          We meticulously control every stage, from roasting the beans to unlock their full flavor potential, to packaging and fast delivery to ensure maximum freshness. We strive not just to sell coffee, but to help you create the ideal coffee ritual at home. That's why, in addition to our premium coffee beans, we also offer a curated selection of accessories and equipment to complement your ritual and allow you to fully savor your favorite beverage. Every product in our range undergoes rigorous quality checking before it reaches you.
         </p>
       </div>
     </section>
